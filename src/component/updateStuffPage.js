ca7f32f0a18c24c544abf9063ff8fc143b22d23d
Phil Davis <phil@factoryz.fr>
2021-11-18 17:02:21 +0100

    almost complete update section

--- src/component/updateStuffPage.js
+++ src/component/updateStuffPage.js
@@ -27,31 +27,40 @@ export default class UpdateStuffPage extends React.Component {
 
   static get propTypes() {
     return {
-      stuff : PropTypes.object
+      stuff: PropTypes.object
     }
   }
 
   async componentDidMount() {
     const array = window.location.pathname.split('/')
     const id = array.pop()
-    const params = {ownerId: ""}
+    const params = {ownerId: "", localisation: ""}
     const stuff = await StuffApiClient.getStuffBy(JSON.parse(localStorage.getItem('jwt')), {_id: id})
-    this.setState({id : stuff.stuff?._id})
-    this.setState({stuffToUpdate : stuff.stuff.stuff[0]})
-    const ownerIdResults = await StuffApiClient.getStuffBy(JSON.parse(localStorage.getItem('jwt')), {params })
+    this.setState({id: stuff.stuff?._id})
+    this.setState({stuffToUpdate: stuff.stuff.stuff[0]})
+    const ownerIdResults = await StuffApiClient.getStuffBy(JSON.parse(localStorage.getItem('jwt')), {params})
     console.log('results', ownerIdResults.stuff.stuff)
     //this.setState({allUsers: ownerIdResults.stuff.stuff})
 
 
-      ownerIdResults.stuff.stuff.filter(user=> user.ownerId).map(async _user=> {
+    ownerIdResults.stuff.stuff.filter(user => user.ownerId).map(async _user => {
       const {user} = await UserApiClient.getUser(_user.ownerId, JSON.parse(localStorage.getItem('jwt')))
-        this.setState(prevState=> {
-          if(!prevState.allUsers.map(user=> user.firstName).includes(user.firstName)) {
-            prevState.allUsers.push(user)
-          }
-          return prevState
-        })
+      this.setState(prevState => {
+        if (!prevState.allUsers.map(user => user.firstName).includes(user.firstName)) {
+          prevState.allUsers.push(user)
+        }
+        return prevState
+      })
     })
+
+    const cityResults = await StuffApiClient.getStuffBy(JSON.parse(localStorage.getItem('jwt')), {params})
+    console.log('cityResluts', cityResults)
+
+    let newCity = cityResults.stuff.stuff.filter(location => location.localisation)
+
+    console.log('newCity', newCity)
+
+
     // const allUsers = await UserApiClient.getAllUsers(JSON.parse(localStorage.getItem('jwt')))
     // this.setState({allUsers: allUsers.users})
     // console.log('allUsers', allUsers)
@@ -79,17 +88,17 @@ export default class UpdateStuffPage extends React.Component {
     const id = this.state.stuffToUpdate._id
     // console.log('id', id)
     // console.log('this.state.stuffToUpdate', this.state.stuffToUpdate)
-    if(this.state.stuffToUpdate !== event.target.value) {
+    if (this.state.stuffToUpdate !== event.target.value) {
       this.state.stuffToUpdate[event.target.name] = event.target.value
-      this.setState({updatedStuff : this.state.stuffToUpdate})
+      this.setState({updatedStuff: this.state.stuffToUpdate})
     }
     try {
       const updateResults = await StuffApiClient.updateOneStuff(JSON.parse(localStorage.getItem('jwt')), id, body)
-      if(updateResults) {
-        this.setState({updateSuccess: true})
-        console.log(updateResults)
+      // console.log('updateResults', updateResults)
+      if (updateResults) {
+        this.setState({updateSuccess: true, selectedUserIndex: event.target.options.selectedIndex})
       }
-    } catch(e) {
+    } catch (e) {
       const error = e.errors
       console.log(error)
       this.setState({updateError: true})
@@ -97,21 +106,23 @@ export default class UpdateStuffPage extends React.Component {
   }
 
   render() {
+    // console.log('selectedUserIndex', this.state.selectedUserIndex)
 // console.log(this.state.allUsers)
 // console.log('this.state.stuffToUpdate', this.state.stuffToUpdate)
     // console.log('this.state.stuffToUpdate.description', this.state.stuffToUpdate)
-    console.log('this.state.updatedStuff', this.state.updatedStuff)
-    console.log('allusers', this.state.allUsers)
-    const testObject = Object.entries(this.state.allUsers).map(user =>
-      user[1]
-    )
-    const testObject2 = Object.values(this.state.allUsers).map(user =>
-        user?.ownerId
-    )
-    console.log(testObject, testObject2)
+    // console.log('this.state.updatedStuff', this.state.updatedStuff)
+    // console.log('allusers', this.state.allUsers)
+    // const testObject = Object.entries(this.state.allUsers).map(user =>
+    //   user[1]
+    // )
+    // const testObject2 = Object.values(this.state.allUsers).map(user =>
+    //     user?.ownerId
+    // )
+    // console.log(testObject, testObject2)
 
 
-    return(
+
+    return (
         this.state.stuffToUpdate ?
             (
                 <>
@@ -119,11 +130,12 @@ export default class UpdateStuffPage extends React.Component {
                     <label className={"updateStuff__form__label__ownerId"}
                            htmlFor={"updateOwnerId"}>OwnerID
                     </label>
-                    <select className={"updateStuff__userChange__select"} name={"ownerId"} value={this.state.stuffToUpdate.ownerId}
-                            onChange={(event) => this._submitUpdate(event)}>
-                      <option name={"ownerId"} value="0">Nobody</option>
-                      {this.state.allUsers.map(user =>
-                          <option value={this.state.stuffToUpdate.ownerId}>{user?.firstName}</option>
+                    <select className={"updateStuff__userChange__select"}
+                            name={"ownerId"}
+                            onChange={(event) => this._submitUpdate(event)}
+                            value={this.state.stuffToUpdate.ownerId}>
+                      {this.state.allUsers.map((user, index) =>
+                          <option value={user._id}>{user?.firstName}</option>
                       )}
                     </select>
                   </div>
@@ -131,33 +143,34 @@ export default class UpdateStuffPage extends React.Component {
                     <label className={"updateStuff__form__label__title"}
                            htmlFor={"updateTitle"}>Title
                     </label>
-                      <input
-                          className={"updateStuff__form__input__title"}
-                          name={"title"}
-                          type="text"
-                          placeholder={"Enter the title"}
-                          value={this.state.stuffToUpdate.title}
-                          onChange={(event) => this._submitUpdate(event)}
-                      />
+                    <input
+                        className={"updateStuff__form__input__title"}
+                        name={"title"}
+                        type="text"
+                        placeholder={"Enter the title"}
+                        value={this.state.stuffToUpdate.title}
+                        // onChange={(event) => this._submitUpdate(event)}
+                    />
                   </div>
                   <div className={"updateStuff__form__container__input"}>
                     <label className={"updateStuff__form__label__price"}
                            htmlFor={"updatePrice"}>Price
                     </label>
-                      <input
-                          className={"updateStuff__form__input__title"}
-                          name={"price"}
-                          type="text"
-                          placeholder={"Enter the Price"}
-                          value={this.state.stuffToUpdate.price}
-                          onChange={(event) => this._submitUpdate(event)}
-                      />
+                    <input
+                        className={"updateStuff__form__input__title"}
+                        name={"price"}
+                        type="text"
+                        placeholder={"Enter the Price"}
+                        value={this.state.stuffToUpdate.price}
+                        onChange={(event) => this._submitUpdate(event)}
+                    />
                   </div>
                   <div className={"updateStuff__form__container__input"}>
                     <label className={"updateStuff__form__label__category"}
                            htmlFor={"updateCategory"}>Category
                     </label>
-                    <select className={"updateStuff__form__category__select"} name={"category"} value={this.state.stuffToUpdate.category}
+                    <select className={"updateStuff__form__category__select"} name={"category"}
+                            value={this.state.stuffToUpdate.category}
                             onChange={(event) => this._submitUpdate(event)}>
                       <option value={undefined} selected={true}>--Category--</option>
                       <option value={CATEGORY_MISCELLANEOUS}>Miscellaneous</option>
@@ -195,35 +208,35 @@ export default class UpdateStuffPage extends React.Component {
                     <label className={"updateStuff__form__label__description"}
                            htmlFor={"updateDescription"}>Description
                     </label>
-                      <input
-                          className={"updateStuff__form__input__textBox"}
-                          name={"description"}
-                          type="text"
-                          placeholder={"Enter the Stuff's Description"}
-                          value={this.state.stuffToUpdate.description}
-                          onChange={(event) => this._submitUpdate(event)}
-                      />
+                    <input
+                        className={"updateStuff__form__input__textBox"}
+                        name={"description"}
+                        type="text"
+                        placeholder={"Enter the Stuff's Description"}
+                        value={this.state.stuffToUpdate.description}
+                        onChange={(event) => this._submitUpdate(event)}
+                    />
                   </div>
                   <div className={"updateStuff__form__label__input"}>
                     <label className={"updateStuff__form__label__city"}
                            htmlFor={"updateCity"}>City
                     </label>
-                      <input
+                    <input
                         className={"updateStuff__form__input__textBox"}
                         name={"localisation"}
                         type="text"
                         placeholder={"Enter a City"}
-                        value={this.state.stuffToUpdate.localisation?.city}
+                        value={this.state.stuffToUpdate.localisation}
                         onChange={(event)=> this._submitUpdate(event)}
-                      />
+                    />
                   </div>
-                  <button onClick={(event)=>this._submitUpdate(event)}>
+                  <button onClick={(event) => this._submitUpdate(event)}>
                     Update
                   </button>
                 </>
-                )
+            )
 
-           : null
+            : null
 
     )
   }
